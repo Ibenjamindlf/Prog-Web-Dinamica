e311a247555a50187f4659e72e68f0447861da56
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-09-13 20:32:41 -0300

    update : ABM Auto

---
+++ Controller/abmAuto.php
@@ -0,0 +1,74 @@
+<?php 
+// ABM de Auto (Alta,Baja,Modificacion)
+// Atributos de auto: Patente,marca,modelo,DniDuenio (4)
+class Auto {
+    // Permite buscar un objeto
+    public function buscar ($param){
+        $where = " true ";
+        if ($param != null){
+            if (isset($param['Patente']))
+                $where .= " and Patente = '".$param['Patente']."'";
+            if (isset($param['Marca']))
+                $where .= " and Marca = '".$param['Marca']."'";
+            if (isset($param['Modelo']))
+                $where .= "and Modelo = '".$param['Modelo']."'";
+            if (isset($param['DniDuenio']))
+                $where .= "and DniDuenio = '".$param['DniDuenio']."'";
+        }
+        $arreglo = Auto::listar($where);
+        return $arreglo;
+    }
+    // Espera como parametro un arreglo asociativo donde las claves coinciden con los nombres de las variables instancias del objeto
+    private function cargarObjeto ($param){
+        $objAuto = null;
+        if (array_key_exists('Patente',$param) and
+        array_key_exists('Marca',$param) and
+        array_key_exists('Modelo',$param) and
+        array_key_exists('DniDuenio',$param)){
+            $objAuto = new Auto();
+            $objAuto->setear(
+                $param['Patente'],
+                $param['Marca'],
+                $param['Modelo'],
+                $param['DniDuenio']
+            );
+        }
+        return $objAuto;
+    }
+    // Corrobora que dentro del arreglo asociativo estan seteados los campos claves
+    private function seteadosCamposClaves($param){
+        $resp = false;
+        if (isset($param['Patente'])){
+            $resp = true;
+        }
+        return $resp;
+    }
+    // Espera como parametro un arreglo asociativo donde las claves coinciden con los nombres de las variables instancias del objeto que son claves
+    private function cargarObjetoConClave($param){
+        $objAuto = null;
+        if (isset($param['Patente'])){
+            $objAuto = new Auto();
+            $objAuto->setear($param['Patente'],null,null,null);
+        }
+        return $objAuto;
+    }
+    // permite modificar un objeto
+    public function modificacion($param){
+        $resp = false;
+        $lista = $this->buscar(['Patente' => $param ['Patente']]);
+        if ($lista != null){
+            $objAuto = new Auto();
+            $objAuto -> setear(
+                                $param['Patente'],
+                                $param['Marca'],
+                                $param['Modelo'],
+                                $param['DniDuenio']);
+            if ($objAuto != null and $objAuto->modificar()){
+                $resp = true;
+            }
+        }
+    return $resp;
+    }
+    // Restantes: baja,alta
+}
+?>

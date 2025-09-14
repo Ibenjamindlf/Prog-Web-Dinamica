c756c33185a1d4e9dd32fe689e822320a89475cb
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-09-14 17:53:49 -0300

    Update : AbmAuto.php
    Se agrego alta y baja al ABM, a demas de corregir modificar y errores de sintaxis

--- Controller/abmAuto.php
+++ Controller/abmAuto.php
@@ -1,7 +1,7 @@
 <?php 
 // ABM de Auto (Alta,Baja,Modificacion)
 // Atributos de auto: Patente,marca,modelo,DniDuenio (4)
-class Auto {
+class AbmAuto {
     // Permite buscar un objeto
     public function buscar ($param){
         $where = " true ";
@@ -11,9 +11,9 @@ class Auto {
             if (isset($param['Marca']))
                 $where .= " and Marca = '".$param['Marca']."'";
             if (isset($param['Modelo']))
-                $where .= "and Modelo = '".$param['Modelo']."'";
+                $where .= " and Modelo = '".$param['Modelo']."'";
             if (isset($param['DniDuenio']))
-                $where .= "and DniDuenio = '".$param['DniDuenio']."'";
+                $where .= " and DniDuenio = '".$param['DniDuenio']."'";
         }
         $arreglo = Auto::listar($where);
         return $arreglo;
@@ -54,21 +54,46 @@ class Auto {
     }
     // permite modificar un objeto
     public function modificacion($param){
-        $resp = false;
-        $lista = $this->buscar(['Patente' => $param ['Patente']]);
-        if ($lista != null){
-            $objAuto = new Auto();
-            $objAuto -> setear(
-                                $param['Patente'],
-                                $param['Marca'],
-                                $param['Modelo'],
-                                $param['DniDuenio']);
-            if ($objAuto != null and $objAuto->modificar()){
-                $resp = true;
-            }
+    $resp = false;
+    $lista = $this->buscar(['Patente' => $param['Patente']]);
+    if ($lista != null){
+        $objAuto = $lista[0]; // tomo el primer resultado
+        $objAuto->setear(
+            $param['Patente'],
+            $param['Marca'],
+            $param['Modelo'],
+            $param['DniDuenio']
+        );
+        if ($objAuto->modificar()){
+            $resp = true;
         }
+    }
     return $resp;
     }
     // Restantes: baja,alta
+    // Permite eliminar un objeto
+    public function baja($param){
+        $resp = false;
+        if ($this->seteadosCamposClaves($param)){
+            $objAuto = $this->cargarObjetoConClave($param);
+            if ($objAuto != null and $objAuto->eliminar()){
+                $resp = true;
+            }
+        }
+        return $resp;
+    }
+    // Permite agregar un objeto
+    public function alta($param){
+        $resp = false;
+        $busquedaAuto = ["Patente" => $param["Patente"]];
+        $existeAuto = $this->buscar($busquedaAuto);
+        if ($existeAuto == null){
+            $objAuto = $this->cargarObjeto($param);
+            if ($objAuto->insertar()){
+                $resp = true;
+            }
+        }
+        return $resp;
+    }
 }
 ?>

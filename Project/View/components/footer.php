da64d895fb88fe936a77d39ac0ad6f1a7a782c76
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-09-14 23:53:15 -0300

    Update footer.php
    Se mejora el footer

--- Project/View/components/footer.php
+++ Project/View/components/footer.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inicio - PWD 2025</title>
-  <link rel="stylesheet" href="./Frameworks/bootstrap.min.css">
-  <link rel="stylesheet" href="./resources/css/styleCarrusel.css">
-</head>
-  <!-- Footer -->
+<!-- Pie redirect index php htdocs -->
   <footer class="bg-primary text-center text-white py-3 mt-auto shadow">
     <small>&copy; 2025 PWD - Todos los derechos reservados</small>
   </footer>
+
+<script src="../assets/function/Bootstrap/"></script>
+<!-- bootsrtap -->
+<script src="../assets/function/Bootstrap/bootstrap.bundle.js"></script>
+<script src="../assets/function/Bootstrap/bootstrap.bundle.min.js"></script>
+<script src="../assets/function/Bootstrap/bootstrap.min.js"></script>
+<script src="../assets/function/Bootstrap/bootstrapValidator.min.js"></script>
+<!-- scripts (a completar)-->
+<script type="text/javascript" src="../assets/function/"></script>
+<script type="text/javascript" src="../assets/function/"></script>
+</body>
+
 </html>

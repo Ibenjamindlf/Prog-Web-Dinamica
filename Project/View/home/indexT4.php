2a8bd39688a90c87c09b3dd8b657b8ff006905c7
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-09-20 01:19:05 -0300

    Create indexT4.php
    Se agrega un index exclusivo para visualizar los ejercicios del trabajo n°4

---
+++ Project/View/home/indexT4.php
@@ -0,0 +1,37 @@
+<?php 
+    $titulo = "Inicio - PWD 2025";
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/PWD_IBENJAMINDLF/Project/View/components/header.php';
+?>
+
+<link rel="stylesheet" href="<?php echo $BASE_URL; ?>Project/View/assets/style/styleCarrusel.css">
+
+  <!-- Header propio -->
+  <header class="container text-center py-5">
+    <h1 class="display-4 fw-bold text-primary">Trabajo Práctico N°4</h1>
+    <p class="lead bg-info text-dark d-inline-block px-4 py-2 rounded-pill mt-3 shadow-sm">
+      📝 Resolución de ejercicios
+    </p>
+  </header>
+
+  <!-- Contenido principal -->
+  <main class="container my-5 text-center">
+    <h2 class="fw-bold mb-4">Ejercicios</h2>
+    <div class="row g-3">
+      <div class="col-md-6">
+        <a class="btn btn-primary btn-lg w-100 shadow" href="https://github.com/Ibenjamindlf/Prog-Web-Dinamica/tree/main/Project/Model">Ejercicio 1</a>
+        <a class="btn btn-primary btn-lg w-100 shadow mt-3" href="https://github.com/Ibenjamindlf/Prog-Web-Dinamica/tree/main/Project/Controller">Ejercicio 2</a>
+        <a class="btn btn-primary btn-lg w-100 shadow mt-3" href="../admin/administrarAutos.php">Ejercicio 3</a>
+        <a class="btn btn-primary btn-lg w-100 shadow mt-3" href="../admin/buscarAuto.php">Ejercicio 4</a>
+      </div>
+      <div class="col-md-6">
+        <a class="btn btn-primary btn-lg w-100 shadow" href="../admin/listaPersonas.php">Ejercicio 5</a>
+        <a class="btn btn-primary btn-lg w-100 shadow mt-3" href="#">Ejercicio 6 (Prox..)</a>
+      </div>
+    </div>
+  </main>
+
+<!-- Footer -->
+<?php
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/PWD_IBENJAMINDLF/Project/View/components/footer.php';
+?>
+

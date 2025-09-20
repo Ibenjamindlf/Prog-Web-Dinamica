4da606366ddfe522ce81e8941b620b3152f4e9a7
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-09-20 01:18:23 -0300

    Update header.php
    Se vinculan los estilos dentro del header para aplicación global

--- Project/View/components/header.php
+++ Project/View/components/header.php
@@ -8,6 +8,7 @@ include_once '../../../configuracion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap css -->
     <link rel="stylesheet" href="/PWD_ibenjamindlf/project/view/assets/style/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/style/styleValidaciones.css">
     <!-- titulo por pagina -->
     <title><?php echo $titulo ?></title>
 </head>
